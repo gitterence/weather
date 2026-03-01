a0399158a6920fd39e0337b97bc528a550e46dfb
gitterence <terencejye@gmail.com>
2026-02-28 18:10:42 -0800

    feat: set up search ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import bgImage from "./assets/weather-bg-image.jpeg"
+import SearchBar from "./components/SearchBar"
 
 function App() {
   return (
-    <>
-      <div>Weather App</div>
-    </>
+    <div className="min-h-screen relative overflow-hidden">
+      {/* Background Image with Overlay */}
+      <div className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        style={{
+          backgroundImage: `url(${bgImage})`
+        }}
+      ></div>
+      <div className="relative z-10 container mx-auto px-4 py-8 min-h-screen">
+        <div className="max-w-7xl mx-auto">
+          {/* Header */}
+          <div className="text-center mb-12">
+            <div className="mb-8">
+              <h1 className="text-5xl md:text-5xl font-bold text-white mb-4 drop-shadow-2xl tracking-tight">
+                Weather{" "}
+                <span className="bg-linear-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent">
+                  Pro
+                </span>
+              </h1>
+              <p className="text-white/80 text-lg md:text-xl mb-8 max-w-2xl mx-auto leading-relaxed">
+                Get the latest weather updates for any location around the world.
+              </p>
+            </div>
+
+            <div className="flex flex-col lg:flex-row items-center justify-center space-y-6 lg:space-y-0 lg:space-x-6 mb-12">
+              <SearchBar />
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,54 @@
+import { MapPin, Search, X } from "lucide-react";
+
+function SearchBar() {
+    return (
+        <div className="relative w-full max-w-2xl">
+            <form className="relative">
+                <div className="relative group">
+                    <Search className="absolute left-4 top-1/2 -translate-y-1/2 
+                    text-gray/60 w-5 h-5 
+                    group-focus-within:text-white transition-all" />
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="w-full pl-12 pr-24 py-4 bg-white/10 backdrop-blur-xl rounded-full 
+                    text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-white/50 
+                    transition-all duration-300 hover:bg-white/20"
+                    />
+                    {/* Conditional Rendering for Clear Button */}
+                    <button className="absolute right-14 top-1/2 transform -translate-y-1/2 
+                    text-white/50 hover:text-white transition-all p-1 rounded-full hover:bg-white/10">
+                        <X className="w-4 h-4" />
+                    </button>
+
+                    {/* <button className="absolute right-14 top-1/2 transform -translate-y-1/2 
+                    text-white/50 hover:text-white transition-all p-1 rounded-full hover:bg-white/10">
+                        <MapPin className="w-5 h-5" />
+                    </button> */}
+                </div>
+            </form>
+
+            {/* Searching */}
+            <div className="absolute top-full left-0 right-0 mt-3 bg-white/10 backdrop-blur-xl 
+            border border-white/20 rounded-2xl shadow-2xl overflow-hidden z-50">
+                <div className="p-6 text-center text-white/70">
+                    <div className="animate-spin rounded-full h-6 w-6 border-2 border-white/30 border-t-white mx-auto"></div>
+                    <p className="animate-pulse mt-2">Searching...</p>
+                </div>
+                {/* ELSE */}
+                <button className="w-full px-6 py-4 text-left hover:bg-white/10 transition-all
+                duration-200 flex items-center justify-between group border-b border-white/10 last:boder-b-0">
+                    <div className="font-medium text-white group-hover:text-white/90">
+                        City Name
+                        <span>,City State</span>
+                    </div>
+                    <div className="text-sm text-white/60">Country</div>
+                    <Search className="w-4 h-4 text-white/40 group-hover:text-white/60 transition-all" />
+                </button>
+
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
